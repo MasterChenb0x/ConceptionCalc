9299f7779e2cbd0b8f91cae3e4d27554b7b9094a
chenb0x <artofmorellc@gmail.com>
2017-06-29 22:38:44 -0700

    Basic month calculations.

--- ConceptionCalc.py
+++ ConceptionCalc.py
@@ -4,3 +4,6 @@ import sys
 
 months = ["January","February","March","April","May","June","July","August","September","October","November","December"]
 
+birthMonth = raw_input("Enter your birth month(1-12):")
+month = int(birthMonth) - 9
+print months[month-1]
--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Conception Calculator  
-
+---
 This is a simple conception calculator. What sets this apart from otherswill become apparent as features are added.
 
-
+###### 06/29/2017
+1. Basic tests
